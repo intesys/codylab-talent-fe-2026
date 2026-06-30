987e7dc5bb4082683ee6bdc44f221e044b34ba36
MichelePozzato <mickipozz@gmail.com>
2026-06-30 17:18:19 +0200

    added all botton css with exemples

--- src/components/projectListItem/ProjectListItem.tsx
+++ src/components/projectListItem/ProjectListItem.tsx
@@ -91,6 +91,16 @@ export default function ProjectListItem({
           <div className={styles.info}>
             <button className={styles.buttonDelete} onClick={() => onDelete(id)}> </button>
           </div>
+          <button className={styles.buttonFilter1}> </button>
+          <button className={styles.buttonDetails}>Visualizza Dettaglio </button>
+          <button className={styles.buttonApply}>Applica</button>
+          <button className={styles.buttonTable}></button>
+          <button className={styles.buttonGraph}></button>
+
+          <button className={styles.buttonExport}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                  <path d="M10.4167 17.5H4.16667C3.72464 17.5 3.30072 17.3244 2.98816 17.0118C2.67559 16.6993 2.5 16.2754 2.5 15.8333V4.16667C2.5 3.72464 2.67559 3.30072 2.98816 2.98816C3.30072 2.67559 3.72464 2.5 4.16667 2.5H15.8333C16.2754 2.5 16.6993 2.67559 17.0118 2.98816C17.3244 3.30072 17.5 3.72464 17.5 4.16667V10.4167M2.5 8.33333H17.5M8.33333 2.5V17.5M13.3333 15.8333H18.3333M15.8333 18.3333L18.3333 15.8333L15.8333 13.3333" stroke="#0F33CE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg> Scarica Report</button>
         </>
       )}
     </li>
