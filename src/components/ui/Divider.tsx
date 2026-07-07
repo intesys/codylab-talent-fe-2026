ea360e053fc9aa93fa5e136c4ace43b59fde6095
DanniaLima <dannia.lima@gmail.com>
2026-07-07 18:03:29 +0200

    Add typescript

--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -1,3 +1,3 @@
 import styles from "./Divider.module.css";
 
-export const Divider = () => {return (<div className={styles.divider}></div>)}
+export const Divider: React.FC = () => {return (<div className={styles.divider}></div>)}
--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -1,3 +1,3 @@
 import styles from "./Section.module.css";
 
-export const Section = ({children}) => {return (<section className={styles.section}>{children}</section>)}
+export const Section: React.FC<React.PropsWithChildren> = ({children}) => {return (<section className={styles.section}>{children}</section>)}
