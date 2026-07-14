0352e2078a5fd2c48f8340a00e41d4563d4bb5c6
DanniaLima <dannia.lima@gmail.com>
2026-07-14 09:25:36 +0200

    feat: adjust format

--- src/components/ui/Divider.tsx
+++ src/components/ui/Divider.tsx
@@ -1,3 +1,5 @@
 import styles from "./Divider.module.css";
 
-export const Divider: React.FC = () => {return (<div className={styles.divider}></div>)}
+export const Divider: React.FC = () => {
+  return <div className={styles.divider}></div>;
+};
