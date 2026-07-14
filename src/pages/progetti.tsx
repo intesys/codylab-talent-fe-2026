6de47dc1b5770f48cb7061230af5478240e7aca0
hribeiro <henrique.ribeiro@intesys.it>
2026-07-14 09:34:44 +0200

    refactor: adjust formatting

--- src/pages/progetti.tsx
+++ src/pages/progetti.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { Project } from "../App";
 import Header from "../components/header/Header";
 import ProjectDetailsCard from "../components/ProjectDetailsCard/ProjectDetailsCard";
